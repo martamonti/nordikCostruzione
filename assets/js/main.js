721a010d152aba8f4a576e8c08794bf324062d6d
martamonti <77634550+martamonti@users.noreply.github.com>
2024-05-01 11:08:00 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".2nav");
+    $nav.toggleClass('scrolled2', $(this).scrollTop() > $nav.height());
+  });
+});
+
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
